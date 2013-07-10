186c1a9dd792a589781430e05c1f55a7f2151281
Michael Connell <mconnell@i4kd.com>
2013-07-10 01:15:15 +0000

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require ('fs');
+//var buf = require('buf');
+var binaryText = fs.readFileSync('./index.html', 'ascii');
+var displayText = binaryText.toString(binaryText, 'ascii');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+//  console.log(displayText);
+    response.send(displayText);
 });
 
 var port = process.env.PORT || 5000;
