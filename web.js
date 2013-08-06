75069dadbd1a82229c168bb625390016f205b948
mconnell2013 <mconnell@i4kd.com>
2013-08-06 02:25:21 +0000

    Revised for hw 6

--- web.js
+++ web.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var fs = require ('fs');
 //var buf = require('buf');
-var binaryText = fs.readFileSync('./index.html', 'ascii');
-var displayText = binaryText.toString(binaryText, 'ascii');
+//var binaryText = fs.readFileSync('./index.html', 'ascii');
+//var displayText = binaryText.toString(binaryText, 'ascii');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
 //  console.log(displayText);
-    response.send(displayText);
+    var html = fs.readFileSync(htmlfile).toString();
+//    response.send(displayText);
+    response.send(html);
 });
 
 var port = process.env.PORT || 8080;
